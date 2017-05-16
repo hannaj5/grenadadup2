c84d7041d94d7e296e7b0da0a6e15bd27bb8d11f
Robert Olendorf <drolendorf@gmail.com>
2017-05-16 20:03:26 +0000

    much work on data display

--- app/models/archeological_site.rb
+++ app/models/archeological_site.rb
@@ -7,6 +7,12 @@ class ArcheologicalSite < ActiveRecord::Base
   has_and_belongs_to_many :previous_work
   has_and_belongs_to_many :threats
   
+  accepts_nested_attributes_for :ceramic_types
+  accepts_nested_attributes_for :ceramic_diagnostics
+  accepts_nested_attributes_for :previous_work
+  accepts_nested_attributes_for :threats
+  
   has_many :maps
   has_many :generic_files
+
 end
